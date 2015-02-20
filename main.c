1478d01950ed5be365d0bb024c50e25acd326e4a
Ciro Simonetti <ciro.simonetti207@gmail.com>
2015-02-20 09:43:56 +0100

    e si committa. . .

--- main.c
+++ main.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char** argv) {
+ int N = 4;
 
+int main(int argc, char** argv) {
+    
+    int i,cont,a;
+  
+    int array[N];
+    a = 1;
+    
+    
+    printf ("iserisci l'età di %d persone:  \n",N);
+    
+    for(i=0;i<N;i++){
+        
+        printf ("iserisci l'età della %da persona:  ",a);
+        
+        
+        scanf ("%d",&(array[i]));
+        
+        a++;
+    }
+    
+    
+    cont = 0;
+    
+    for (i=0;i<N;i++){
+        
+        if(array[i]>=18){
+            
+            
+            cont++ ;
+        
+        }
+        
+        
+        
+    }
+     printf ("i maggiorenni sono : %d",cont);
+    
+    return 0;
     return (EXIT_SUCCESS);
 }
 
